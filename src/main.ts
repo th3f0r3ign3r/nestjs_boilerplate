27a34340ed5fff3fea47f3e2ed4731d7c7bbe5d8
Ronel KPOSSOU <ronel.kpossou@epitech.eu>
2022-09-26 11:16:12 +0100

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
