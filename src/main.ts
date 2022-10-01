9516a1cefd8cf0dd66d392a874b401f197710903
Ronel KPOSSOU <ronel.kpossou@epitech.eu>
2022-10-01 19:41:59 +0100

    push helmet to fist

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  app.use(helmet());
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
