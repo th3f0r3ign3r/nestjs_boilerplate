757504513960b307440fd5312231e0bb9b5c2798
Ronel KPOSSOU <ronel.kpossou@epitech.eu>
2022-10-01 20:27:41 +0100

    [fix] misconfigured csrf (deprecated)

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  app.use(csurf());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(process.env.PORT || 5000);
