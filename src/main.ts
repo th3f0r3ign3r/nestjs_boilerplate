32035071cfe430318efe3e838d038cc609398e01
Ronel KPOSSOU <ronel.kpossou@epitech.eu>
2022-10-01 19:51:22 +0100

    add CSRF protection security

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as csurf from 'csurf';
 import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  app.use(csurf());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(process.env.PORT || 5000);
