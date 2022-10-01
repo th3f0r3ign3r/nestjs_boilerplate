67d79bdc91b98bb6ca4353f51df1d7a899fe47f1
Ronel KPOSSOU <ronel.kpossou@epitech.eu>
2022-10-01 19:32:18 +0100

    add helmet package for http query security

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.use(helmet());
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
