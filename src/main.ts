9e96429b301c018465f8a96a6f5e434c52e10f7f
Ronel KPOSSOU <ronel.kpossou@epitech.eu>
2022-10-03 15:43:49 +0100

    add data output compression

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
+import * as compression from 'compression';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(helmet());
-  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.use(helmet());
+  app.use(compression());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
